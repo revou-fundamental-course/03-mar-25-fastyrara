6be25962834c96aa5f9b613193a284cc20a950f4
fastyrara <fastynirawati@gmail.com>
2025-03-13 17:20:25 +0700

    finishing yg ngga finish

--- js/script.js
+++ js/script.js
@@ -1,20 +1,29 @@
 // ini js
 console.log("Javascript is working")
 
-// Function to validate form
-function validateForm(){
-    // Get value from input
-    const nameInput=document.getElementById("name-input");
-    console.log(nameInput.value);
+// Function to validate form kak yama
+// function validateForm(){
+//     // Get value from input
+//     const nameInput=document.getElementById("name-input");
+//     console.log(nameInput.value);
     
-    // Check if input is empty
-    if(nameInput.value==""){
-        alert("Name must be filled out")
-    } else {
-        // Write value to the page
-    }
+//     // Check if input is empty
+//     if(nameInput.value==""){
+//         alert("Name must be filled out")
+//     } else {
+//         // Write value to the page
+//     }
+// }
+
+// Ganti nama sambutan
+function replaceName(){
+    let user=prompt("Please enter your name","");
+    document.getElementById("user").innerHTML=user;
 }
 
+replaceName();
+
+// Banner autoslide
 let bannerIndex=0;
 showBanner();
 
@@ -45,3 +54,28 @@ function showBanner(){
 
 // Set interval to change banner
 setInterval(nextBanner,3000);
+
+// Function untuk validate form baru
+function validateForm(){
+    const name=document.forms["message-form"]["full-name"].value;
+    const birthDate=document.forms["message-form"]["birth-date"].value;
+    const gender=document.forms["message-form"]["gender"].value;
+    const messages=document.forms["message-form"]["messages"].value;
+
+    // Check if input is empty
+    if(name==""||birthDate==""||gender==""||messages==""){
+        alert("Tidak boleh ada yang kosong");
+        return false;
+    }
+
+    setSenderUI(name,birthDate,gender,messages);
+
+    return false;
+}
+
+function setSenderUI(name,birthDate,gender,messages){
+    document.getElementById("sender-full-name").innerHTML=name;
+    document.getElementById("sender-birth-date").innerHTML=birthDate;
+    document.getElementById("sender-gender").innerHTML=gender;
+    document.getElementById("sender-messages").innerHTML=messages;
+}
